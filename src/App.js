cd716fbd3b11482a23fb1140017a39418a8fda88
tejas <sadadetej.27@gmail.com>
2023-07-17 23:13:12 +0530

    Doing Network calls and andling response the right

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {  useSelector } from 'react-redux/es/hooks/useSelector';
+import { useEffect } from 'react';
 
 function App() {
+  const cart=useSelector(state=> state.cart)
   const showCart= useSelector(state=> state.ui.cartIsVisible)
+
+  useEffect(() => {
+    fetch("https://redux-cart-a21e2-default-rtdb.firebaseio.com/cart.json",{
+      method:"PUT",
+      body:JSON.stringify(cart),
+    })
+  }, [cart])
+  
   return (
     <Layout>
       {showCart && <Cart />}
